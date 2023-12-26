c7ea4ea0a05d187d68285c3a28b3e006eddc5839
goshva <sha.egor@gmail.com>
2023-12-26 05:45:42 +0300

    fix  sent auto format telega errors

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ parallax = document.getElementsByClassName('parallax')[0]
     vh = document.documentElement.clientHeight * 0.65;
     parallax.addEventListener('scroll', function(e) {
         console.log(parallax.scrollTop);
-        city_front.style.height = ${2*parallax.scrollTop+ vh}px 
+        city_front.style.height = `${2*parallax.scrollTop+ vh}px `
     })
